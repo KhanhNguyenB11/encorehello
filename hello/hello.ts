86a8398a04e5bebe30088748df32db6ad02e1581
KhanhNguyen <khanh.anh.nguyen@agest.vn>
2025-03-26 15:22:39 +0700

    remove test

--- hello/hello.ts
+++ hello/hello.ts
@@ -15,7 +15,7 @@ import { api } from "encore.dev/api";
 export const get = api(
   { expose: true, method: "GET", path: "/hello/:name" },
   async ({ name }: { name: string }): Promise<Response> => {
-    const msg = `Hello ${name}2!`;
+    const msg = `Hello ${name}! ahihi`;
     return { message: msg };
   }
 );
